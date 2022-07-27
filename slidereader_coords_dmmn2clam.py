52d6438cdca1c2af14334bd413bf85b97dc57e25
micGuerr <michele.guerreri@gmail.com>
2022-07-27 14:25:30 +0200

    Modified file to make the parsing input parameter changes effective

--- slidereader_coords_dmmn2clam.py
+++ slidereader_coords_dmmn2clam.py
@@ -18,25 +18,33 @@ parser.add_argument( "--otsu", nargs="?", type=bool, default=False)
 if __name__ == '__main__':
 
     args = parser.parse_args()
-    
-    slides_to_read = ["testing_images/1.svs","testing_images/2.svs","testing_images/3.svs"] # list of testing whole slide images
-    coord_file = open('test_coords.csv', 'w') # a file listing all patch coordinates
 
-    ### get coordinates of tissue patches in whole slide images using Otsu algorithm ###
-    # for image_path in slides_to_read:
-    #     slide = openslide.OpenSlide(image_path)
-    #     slide_id = os.path.splitext(os.path.basename(image_path))[0]
-    #     grid, _ = extract_tissue.make_sample_grid(slide, 256,
-    #            20, 10, 10, False, prune=False, overlap=0)
-    #     for (x,y) in grid:
-    #         coord_file.write('{},{},{},0\n'.format(slide_id, x, y))
+    source = args.source
+    slides = sorted(os.listdir(source))
+    slides = [slide for slide in slides if os.path.isfile(os.path.join(source, slide))]
+    #slides_to_read = ["testing_images/1.svs","testing_images/2.svs","testing_images/3.svs"] # list of testing whole slide images
 
-    ### get coordinates of all patches in whole slide images ###
-    stride = 256
-    for image_path in slides_to_read:
-        slide = openslide.OpenSlide(image_path)
-        slide_id = os.path.splitext(os.path.basename(image_path))[0]
-        image_size = slide.dimensions
-        for ii in range(0,image_size[0],stride):
-            for jj in range(0,image_size[1],stride):
-                coord_file.write('{},{},{},0\n'.format(slide_id, ii, jj))
+    coord_file_path = args.out_path
+    coord_file = open(coord_file_path, 'w') # a file listing all patch coordinates
+
+    do_otsu = args.otsu
+
+    if do_otsu:
+        ### get coordinates of tissue patches in whole slide images using Otsu algorithm ###
+        for image_path in slides:
+            slide = openslide.OpenSlide(os.path.join(source,image_path))
+            slide_id = os.path.splitext(os.path.basename(image_path))[0]
+            grid, _ = extract_tissue.make_sample_grid(slide, 256,
+                   20, 10, 10, False, prune=False, overlap=0)
+            for (x,y) in grid:
+                coord_file.write('{},{},{},0\n'.format(slide_id, x, y))
+    else:
+        ### get coordinates of all patches in whole slide images ###
+        stride = 256
+        for image_path in slides:
+            slide = openslide.OpenSlide(os.path.join(source, image_path))
+            slide_id = os.path.splitext(os.path.basename(image_path))[0]
+            image_size = slide.dimensions
+            for ii in range(0,image_size[0],stride):
+                for jj in range(0,image_size[1],stride):
+                    coord_file.write('{},{},{},0\n'.format(slide_id, ii, jj))
