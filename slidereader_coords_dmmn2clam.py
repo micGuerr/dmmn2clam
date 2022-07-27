42e5ec98513ff39817c8b843a3208f1adf430dce
micGuerr <michele.guerreri@gmail.com>
2022-07-27 12:09:11 +0200

    Added two extra files from CLAM with the file name already modified

--- slidereader_coords_dmmn2clam.py
+++ slidereader_coords_dmmn2clam.py
@@ -15,20 +15,20 @@ if __name__ == '__main__':
     coord_file = open('test_coords.csv', 'w') # a file listing all patch coordinates
 
     ### get coordinates of tissue patches in whole slide images using Otsu algorithm ###
-    for image_path in slides_to_read:
-        slide = openslide.OpenSlide(image_path)
-        slide_id = os.path.splitext(os.path.basename(image_path))[0]
-        grid, _ = extract_tissue.make_sample_grid(slide, 256,
-               20, 10, 10, False, prune=False, overlap=0)
-        for (x,y) in grid:
-            coord_file.write('{},{},{},0\n'.format(slide_id, x, y))
-
-    ### get coordinates of all patches in whole slide images ###
-    # stride = 256
     # for image_path in slides_to_read:
     #     slide = openslide.OpenSlide(image_path)
     #     slide_id = os.path.splitext(os.path.basename(image_path))[0]
-    #     image_size = slide.dimensions
-    #     for ii in range(0,image_size[0],stride):
-    #         for jj in range(0,image_size[1],stride):
-    #             coord_file.write('{},{},{},0\n'.format(slide_id, ii, jj))
+    #     grid, _ = extract_tissue.make_sample_grid(slide, 256,
+    #            20, 10, 10, False, prune=False, overlap=0)
+    #     for (x,y) in grid:
+    #         coord_file.write('{},{},{},0\n'.format(slide_id, x, y))
+
+    ### get coordinates of all patches in whole slide images ###
+    stride = 256
+    for image_path in slides_to_read:
+        slide = openslide.OpenSlide(image_path)
+        slide_id = os.path.splitext(os.path.basename(image_path))[0]
+        image_size = slide.dimensions
+        for ii in range(0,image_size[0],stride):
+            for jj in range(0,image_size[1],stride):
+                coord_file.write('{},{},{},0\n'.format(slide_id, ii, jj))
